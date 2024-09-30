0adcd4855e5a0ec5774b070e990ec49ec66bda14
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-09-30 20:53:07 +0330

    added mixitup filter js

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,3 +37,24 @@ const scrollHeader = () => {
     : header.classList.remove("scroll-header");
 };
 window.addEventListener("scroll", scrollHeader);
+
+/* ==========  MIXITUP FILTER PRODUCTS(added from configuration of main website)  ========== */
+let mixerProducts = mixitup(".products__content", {
+  selectors: {
+    target: ".products__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
+/*Default filter products*/
+mixerProducts.filter(".delicacies");
+/*Link active products*/
+const linkProducts = document.querySelectorAll(".products__item");
+
+function activeProducts() {
+  linkProducts.forEach((l) => l.classList.remove("active-product"));
+  this.classList.add("active-product");
+}
+
+linkProducts.forEach((l) => l.addEventListener("click", activeProducts));
